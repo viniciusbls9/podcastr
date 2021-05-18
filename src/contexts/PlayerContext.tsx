d36ff861f23c2d89404afb5444d7ab66e0d40d57
Vinicius Benedito <beneditoloura@gmail.com>
2021-05-18 08:25:01 -0300

    podcastr

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,123 @@
+import { inflate } from 'node:zlib';
+import { createContext, useState, ReactNode, useContext } from 'react';
+
+type Episode = {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+}
+
+type PlayerContextData = {
+    episodeList: Episode[]
+    play: (episode: Episode) => void;
+    setPlayingState: (state: boolean) => void;
+    playList: (list: Episode[], index: number) => void;
+    playNext: () => void;
+    playPrevious: () => void;
+    togglePlay: () => void;
+    toggleLoop: () => void;
+    toggleShuffle: () => void;
+    clearPlayingState: () => void;
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    isShuffling: boolean;
+    hasNext: boolean;
+    hasPrevious: boolean;
+    isLooping: boolean;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+    children: ReactNode;
+}
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isLooping, setIsLooping] = useState(false);
+    const [isShuffling, setIsShuffling] = useState(false);
+
+    function play(episode: Episode) {
+        setEpisodeList([episode])
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true)
+    }
+
+    function playList(list: Episode[], index: number) {
+        setEpisodeList(list);
+        setCurrentEpisodeIndex(index);
+        setIsPlaying(true);
+    }
+
+    function toggleLoop() {
+        setIsLooping(!isLooping)
+    }
+
+    function toggleShuffle() {
+        setIsShuffling(!isShuffling)
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying)
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state)
+    }
+
+    function clearPlayingState() {
+        setEpisodeList([])
+        setCurrentEpisodeIndex(0)
+    }
+
+    const hasPrevious = currentEpisodeIndex > 0;
+    const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
+
+    function playNext() {
+        if(isShuffling) {
+            const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+            setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+        } else if(hasNext) {
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+        }
+    }
+
+    function playPrevious() {
+        if(hasPrevious) {
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+        }
+    }
+
+    return (
+        <PlayerContext.Provider
+            value={{
+                episodeList,
+                currentEpisodeIndex,
+                play,
+                isPlaying,
+                isShuffling,
+                togglePlay,
+                setPlayingState,
+                playList,
+                playNext,
+                playPrevious,
+                hasNext,
+                hasPrevious,
+                toggleLoop,
+                isLooping,
+                toggleShuffle,
+                clearPlayingState
+                }}
+        >
+            {children}
+        </PlayerContext.Provider>
+    )
+}
+
+export const usePlayer = () => {
+    return useContext(PlayerContext);
+}
